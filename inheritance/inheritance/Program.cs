58abf324a3373e989c5bb33c972e40d2f701d766
samba@SAMS_HP <Sosorbaram048@revature.net>
2022-07-29 08:18:52 -0700

    added inheritance file

---
+++ inheritance/inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
