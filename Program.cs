3c23ef6290284fdcc5f9d426856cb352d120f48c
Sosorbaram Bayaraa <sosorbaram048@revature.net>
2022-08-01 09:24:31 -0700

    added HackerRank

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SosorbaramBayaraaRevature1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
