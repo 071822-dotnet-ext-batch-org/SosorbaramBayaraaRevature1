8e907c54ba644e31fc351d1e9377f23b728f7163
samba@SAMS_HP <Sosorbaram048@revature.net>
2022-08-05 10:56:32 -0700

    created SQL file

---
+++ SQL/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+
+
+namespace SQL
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Assumes connectionString is a valid connection string.  
+            using (SqlConnection connection = new SqlConnection(""))  
+            {  
+                connection.Open();
+                // Do work here.  
+                SqlCommand command = new SqlCommand("SELECT * from dbo.Customer", connection);
+                SqlDataReader myReader = command.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    Console.WriteLine($"{myReader.GetInt32(0)} \t\t{myReader.GetString(1)}");
+                }
+
+                connection.Close();
+            } 
+        }
+    }
+
+}
