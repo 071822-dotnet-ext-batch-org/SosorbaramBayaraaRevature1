d5ea49f18c058801fa5dd40ab6158d2300aa2257
samba@SAMS_HP <Sosorbaram048@revature.net>
2022-08-10 07:38:04 -0700

    changes

---
+++ HTTP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+namespace HTTP
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+           GetRequest("http://api.icndb.com/jokes/random");
+          Console.ReadKey();
+        }
+        async static void GetRequest(string url)
+        {
+           {    //Created client object, this will initiate requests to the server.
+                // SInce this client is disposable, we used code of block "Using". Once this client object in no use it will automatically dispose
+                using(HttpClient client = new HttpClient())
+                {
+                   using (HttpResponseMessage response =  await client.GetAsync("http://api.icndb.com/jokes/random"))
+                   {
+                        using(HttpContent content = response.Content)
+                        {
+                            string myContent = await content.ReadAsStringAsync();
+                            Console.WriteLine(myContent);
+                        }
+                   }
+                }
+           }
+        }
+    }
+}
