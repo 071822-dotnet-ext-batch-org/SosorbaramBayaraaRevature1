3406e10498dcb8ae1f27f85f86733aa9671e3409
samba@SAMS_HP <Sosorbaram048@revature.net>
2022-08-17 10:02:14 -0700

    Created Employee and Ticket classes iside of Models

---
+++ ProjectOne/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Models;
+public class Employee
+{
+    public Guid EmployeeID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public bool IsManager { get; set; }
+}
---
+++ ProjectOne/Models/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Ticket
+    {
+        public Guid TicketID { get; set; }
+        public Guid FK_EmployeeID { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public int Status { get; set; }
+    }
+}
