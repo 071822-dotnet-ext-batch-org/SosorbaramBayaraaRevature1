5549db94beb3d9c5dfb8f2c0ea4798baa76f59ce
samba@SAMS_HP <Sosorbaram048@revature.net>
2022-08-24 21:44:22 -0700

    Requirement #1 completed

--- ProjectOne/RepoLayer/ProjectOneRepoLayer.cs
+++ ProjectOne/RepoLayer/ProjectOneRepoLayer.cs
@@ -7,7 +7,30 @@ namespace RepoLayer;
 
 public class ProjectOneRepoLayer
 {
+    /// <summary>
+    /// #1 Login
+    /// </summary>
+    /// <param name="login"></param>
+    /// <returns></returns>
+    public async Task<LoginDto> LoginAsync(LoginDto login)
+    {
+        SqlConnection conn1 = new SqlConnection("Server=tcp:revature.database.windows.net,1433;Initial Catalog=Project1;Persist Security Info=False;User ID=samRevature;Password=Hulanlove23;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        using (SqlCommand command = new SqlCommand($"SELECT UserName, Password FROM Employees WHERE UserName = @username AND Password = @password", conn1))
+        {
+            command.Parameters.AddWithValue("@username", login.UserName);
+            command.Parameters.AddWithValue("@password", login.Password);
+            conn1.Open();
+            SqlDataReader ret = await command.ExecuteReaderAsync();
 
+            if (ret.Read())
+            {
+                LoginDto login1 = new LoginDto(ret.GetString(0), ret.GetString(1));
+                return login1;
+            }
+            conn1.Close();
+            return null;
+        }
+    }
     public async Task<List<Ticket>> TicketsAsync(int status)
     {
         // made a connection wusing Sql connection class
@@ -103,6 +126,6 @@ public class ProjectOneRepoLayer
 
         
     }
-
+  
 }
 
