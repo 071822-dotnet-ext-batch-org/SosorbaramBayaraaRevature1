f01849828c5adff5a2988d085c9e8e4fd8d4a5bf
(Sam) Sosorbaram Bayaraa <sosorbaram048@revature.net>
2022-09-10 23:48:42 -0700

    added create and update button

---
+++ Project2/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,101 @@
+﻿using DataAccessLayer.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Project2.DataAccessLayer.RepoLayer;
+
+namespace Project2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class CategoryController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            CategoryVM categoryVM = new CategoryVM();
+            categoryVM.categories = _unitOfWork.Category.GetAll();
+            return View(categoryVM);
+        }
+
+        [HttpGet]
+        public IActionResult CreateUpdate(int? productId)
+        {
+            CategoryVM vm = new CategoryVM();
+            if (productId == null || productId == 0)
+            {
+                return View(vm);
+            }
+            else
+            {
+                vm.Category = _unitOfWork.Category.GetT(x => x.ProductId == productId);
+                if (vm.Category == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(vm);
+                }
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateUpdate(CategoryVM vm)
+        {
+            if (ModelState.IsValid)
+            {
+                if (vm.Category.ProductId == 0)
+                {
+                    _unitOfWork.Category.Add(vm.Category);
+                    TempData["success"] = "Category Created Done!";
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(vm.Category);
+                    TempData["success"] = "Category Update Done!";
+                }
+
+                _unitOfWork.Save();
+
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _unitOfWork.Category.GetT(x => x.ProductId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult DeleteData(int? id)
+        {
+            var category = _unitOfWork.Category.GetT(x => x.ProductId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Delete(category);
+            _unitOfWork.Save();
+            TempData["success"] = "Category Deleted Done!";
+            return RedirectToAction("Index");
+        }
+    }
+}
